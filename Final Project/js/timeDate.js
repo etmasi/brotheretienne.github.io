230e0432767ccd03163ae2ce148d589ab5d71397
etmasi <hesalwayswithme61@gmail.com>
2020-12-20 15:43:36 +0200

    update

--- Final Project/js/timeDate.js
+++ Final Project/js/timeDate.js
@@ -1,4 +1,4 @@
-var now = new Date();
+var now = new lastUpdate();
 var Weekday = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
 var Month = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
 document.write(Weekday[now.getDay()] + ", " + Month[now.getMonth()] + " " + now.getDate() + ", " + now.getFullYear());
